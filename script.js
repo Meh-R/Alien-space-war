7de84c2181938a443497d15d2135889a2ffda3fc
mehdi <rabellemehdi@gmail.com>
2024-03-17 18:09:32 +0100

    invader deplacement colisionbord et vaiseau

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/////////////////////////////
+/////////////////////////////////
+///////////////////////////////////Class forme
+///////////////////////////////////
+///////////////////////////////////
+
 class Forme {
   constructor(width, height, positionX, positionY, couleur, radius, id) {
     this.width = width;
@@ -8,7 +14,7 @@ class Forme {
     this.radius = radius;
     this.id = id;
   }
-
+  //////methode d'affichage
   display() {
     let container = document.querySelector(".container");
     let item = document.createElement("div");
@@ -24,13 +30,94 @@ class Forme {
   }
 }
 
+/////////////////////////////
+/////////////////////////////////
+///////////////////////////////////Class invader
+///////////////////////////////////
+///////////////////////////////////
+
+class Invader extends Forme {
+  constructor(
+    width,
+    height,
+    positionX,
+    positionY,
+    couleur,
+    radius,
+    id,
+    deplacementX,
+    deplacementY,
+    direction
+  ) {
+    super(width, height, positionX, positionY, couleur, radius, id);
+    this.deplacementX = deplacementX;
+    this.deplacementY = deplacementY;
+    this.direction = direction;
+  }
+
+  callBackMove = () => {
+    let id = document.getElementById(this.id);
+
+    //////deplacement invader
+
+    this.positionX += this.deplacementX;
+    this.positionY += this.deplacementY;
+    if (this.positionY - this.height > positionContainer.bottom) {
+      id.style.display = "none";
+      return;
+    } else if (this.positionX + this.deplacementX > positionContainer.right) {
+      this.deplacementX = -this.deplacementX;
+    } else if (this.positionX + this.deplacementX < positionContainer.left) {
+      this.deplacementX = -this.deplacementX;
+    } else {
+      this.deplacementX;
+    }
+
+    id.remove();
+    this.display();
+
+    ///////colision invader ships
+
+    let centerX = this.positionX + this.width / 2;
+    let centerY = this.positionY + this.height / 2;
+
+    if (this.positionY + this.deplacementY > positionContainer.bottom) {
+      id.remove();
+    }
+
+    if (
+      centerX > spaceShips.positionX &&
+      centerX < spaceShips.positionX + spaceShips.width &&
+      centerY > spaceShips.positionY &&
+      centerY < spaceShips.positionY + spaceShips.height
+    ) {
+      alert("you loose");
+    }
+  };
+
+  ////////methode déplacement
+  deplacement() {
+    setInterval(this.callBackMove, randomNumberMinMax(10, 20));
+  }
+}
+
+/////////////////////////////
+/////////////////////////////////
+///////////////////////////////////spaceShips
+///////////////////////////////////
+///////////////////////////////////
+
+//////taille du conteneur
 let container = document.querySelector(".container");
 let positionContainer = container.getBoundingClientRect();
-let containerWidth = positionContainer.right - positionContainer.left;
+let containerWidth = positionContainer.right;
+
+/////position de depart ships
 let shipsStartPositionX =
   (positionContainer.right + positionContainer.left) / 2;
 let shipsStartPositionY = positionContainer.bottom;
 
+/////creation de ships
 let spaceShips = new Forme(
   80,
   50,
@@ -41,14 +128,9 @@ let spaceShips = new Forme(
   2
 );
 
-function randomNumber(number) {
-  return Math.floor(Math.random() * number);
-}
-
 spaceShips.display();
 
-console.log(spaceShips.positionX);
-
+//////////déplacement de ships
 document.addEventListener("keydown", (e) => {
   if (e.keyCode == 39) {
     if (spaceShips.positionX < positionContainer.right - spaceShips.width / 2) {
@@ -66,7 +148,59 @@ document.addEventListener("keydown", (e) => {
   id2.remove();
 
   spaceShips.display();
-  console.log(spaceShips.positionX);
 });
 
-let invader = new Forme();
+/////////////////////////////
+/////////////////////////////////
+///////////////////////////////////randomNumber
+///////////////////////////////////
+///////////////////////////////////
+
+function randomNumber(number) {
+  return Math.floor(Math.random() * number);
+}
+
+function randomNumberMinMax(Min, Max) {
+  return Math.floor(Math.random() * (Max - Min) + Min);
+}
+
+////////////////////////////////
+/////////////////////////////////
+//////////////////////////////// invader
+////////////////////////////////
+//////////////////////////////////
+
+///////function pour directionX l'invader
+function plusOuMoin() {
+  var randomNum = randomNumber(10);
+
+  if (randomNum < 5) {
+    return -5;
+  } else {
+    return 5;
+  }
+}
+
+//////creation de l'invader
+function creatInvader() {
+  let invader = new Invader(
+    50,
+    50,
+    randomNumber(positionContainer.right),
+    -50,
+    "red",
+    "40px",
+    randomNumber(10000),
+    plusOuMoin(),
+    5,
+    "none"
+  );
+  invader.display();
+  invader.deplacement();
+}
+
+setInterval(creatInvader, 1000);
+
+// invader.display();
+
+// invader.deplacement();
